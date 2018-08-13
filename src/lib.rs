62c9b130b6c2c631182f1e4b6f794146a67fcd50
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2018-08-13 19:20:13 +0200

    separate tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+mod tests;
+
 pub mod xor_distance {
     pub fn distance(a: &[u8], b: &[u8]) -> Vec<u8> {
         let a_len: usize = a.len();
@@ -38,65 +41,5 @@ pub mod xor_distance {
 
     pub fn eq (a: &[u8], b: &[u8]) -> bool {
         compare(a, b) == 0
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    #[should_panic(expected = "inputs must be of same length")]
-    fn distance_panic () {
-        let a = vec![ 36, 44, 99 ];
-        let b = vec![ 77, 88 ];
-        super::xor_distance::distance(&a, &b);
-    }
-    #[test]
-    fn distance () {
-        let a = vec![ 36, 44 ];
-        let b = vec![ 77, 88 ];
-        assert_eq!(super::xor_distance::distance(&a, &b), vec![ 105, 116 ]);
-    }
-    #[test]
-    #[should_panic(expected = "inputs must be of same length")]
-    fn compare_panic () {
-        let a = vec![ 36, 44, 99 ];
-        let b = vec![ 77, 88 ];
-        super::xor_distance::compare(&a, &b);
-    }
-    #[test]
-    fn compare_lt () {
-        let a = vec![ 36, 44 ];
-        let b = vec![ 77, 88 ];
-        assert_eq!(super::xor_distance::compare(&a, &b), -1);
-    }
-    #[test]
-    fn compare_gt () {
-        let a = vec![ 99, 44 ];
-        let b = vec![ 77, 88 ];
-        assert_eq!(super::xor_distance::compare(&a, &b), 1);
-    }
-    #[test]
-    fn compare_eq () {
-        let a = vec![ 36, 44 ];
-        let b = vec![ 36, 44 ];
-        assert_eq!(super::xor_distance::compare(&a, &b), 0);
-    }
-    #[test]
-    fn lt () {
-        let a = vec![ 36, 44 ];
-        let b = vec![ 77, 44 ];
-        assert_eq!(super::xor_distance::lt(&a, &b), true);
-    }
-    #[test]
-    fn gt () {
-        let a = vec![ 99, 44 ];
-        let b = vec![ 77, 44 ];
-        assert_eq!(super::xor_distance::gt(&a, &b), true);
-    }
-    #[test]
-    fn eq () {
-        let a = vec![ 99, 44 ];
-        let b = vec![ 99, 44 ];
-        assert_eq!(super::xor_distance::eq(&a, &b), true);
     }
 }
